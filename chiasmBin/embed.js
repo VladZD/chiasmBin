131bf29d14a0d017420538240e00235129338ba4
Vlad Drozd <vladbbird@gmail.com>
2017-09-26 13:47:52 +0300

    Uglify embed.js

--- chiasmBin/embed.js
+++ chiasmBin/embed.js
@@ -1,152 +1,21 @@
-(function() {
-  if (window.__is_c2k_listener_set__) {
-    window.postMessage('c2k_embed_loaded', '*');
-    return;
-  }
-
-  var getStyle = function(elem) {
-    return window.getComputedStyle ? getComputedStyle(elem, "") : elem.currentStyle;
-  };
-  function C2KIframe(elem) {
-    this.elem = elem;
-    this.isHidden = false;
-    this.placeholder = null;
-  }
-  C2KIframe.prototype.showIframePlaceholder = function(iframeHeight) {
-    var placeholder = this.placeholder;
-    if (!placeholder) {
-      placeholder = document.createElement('div');
-      placeholder.style.border = '3px dashed #adadad';
-      placeholder.style.boxSizing = 'border-box';
-      placeholder.style.backgroundColor = "#f1f1f1";
-      placeholder.style.width = this.elem.style.width;
-      this.placeholder = placeholder;
-    }
-    placeholder.style.height = iframeHeight;
-    placeholder.style.display = getStyle(this.elem).display === 'block' ? 'block' : 'inline-block';
-    this.elem.parentElement.insertBefore(placeholder, this.elem);
-  };
-  C2KIframe.prototype.hideIframePlaceholder = function() {
-    if (!this.placeholder || !this.placeholder.parentElement) return;
-    this.placeholder.parentElement.removeChild(this.placeholder);
-  };
-  C2KIframe.prototype.hideIframe = function() {
-    this.elem.style.height = '0';
-    this.isHidden = true;
-  };
-  C2KIframe.prototype.showIframe = function(height) {
-    this.elem.style.height = height;
-    this.isHidden = false;
-  };
-  var createIframe = function(options) {
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('frameborder', '0');
-    iframe.style.border = "0";
-    if (options.width !== undefined) {
-      iframe.style.width = options.width + (options.percent === 'yes' ? '%' : 'px');
-    } else {
-      iframe.style.width = "100%";
-    }
-    iframe.src = options.protocol + '//' + DOWNLOAD_LINK + options.id + '.html?auto';
-    return iframe;
-  };
-  var getScriptParams = function(url) {
-    url = decodeURIComponent(url);
-    var regexp = /(\w+)=([^&]+)/g;
-    var params = {}, result;
-    while (result = regexp.exec(url)) {
-      params[result[1]] = result[2];
-    }
-    var protocol = url.match(/^https?:/);
-    params.protocol = protocol ? protocol[0] : '';
-    return params;
-  };
-  var scriptLoaded = function(script, value) {
-    if (value !== undefined) {
-      script.c2k_is_loaded = value;
-    } else {
-      return script.c2k_is_loaded;
-    }
-  };
-  var getNewlyLoadedScript = function() {
-    var selectors = [
-      'script[src^="http://' + URL + '"]',
-      'script[src^="https://' + URL + '"]',
-      'script[src^="//' + URL + '"]'
-    ];
-    var scripts = document.querySelectorAll(selectors.join(', '));
-    for (var i = 0; i < scripts.length; i++ ) {
-      if (!scriptLoaded(scripts[i])) {
-        return scripts[i];
-      }
-    }
-  };
-  var loadIframe = function() {
-    var script = getNewlyLoadedScript();
-    if (!script) return;
-    scriptLoaded(script, true);
-    var params = getScriptParams(script.src);
-    var c2kIframe = new C2KIframe(createIframe(params));
-    frames.push(c2kIframe);
-    script.parentElement.insertBefore(c2kIframe.elem, script);
-  };
-  var getIframeByWindow = function(iframeWindow) {
-    for (var i = 0; i < frames.length; i++ ) {
-      if (frames[i].elem.contentWindow === iframeWindow) {
-        return frames[i];
-      }
-    }
-  };
-  var iframeHeightListener = function(e, height) {
-    var c2kIframe = getIframeByWindow(e.source);
-    if (!c2kIframe) return;
-    if (height === 'none') {
-      var prevHeight = c2kIframe.elem.style.height;
-      c2kIframe.hideIframe();
-      c2kIframe.showIframePlaceholder(prevHeight);
-    } else if (c2kIframe.isHidden) {
-      c2kIframe.hideIframePlaceholder();
-      c2kIframe.showIframe(height);
-    } else {
-      c2kIframe.elem.style.height = height;
-    }
-  };
-  var iframePopupListener = function(e) {
-    var c2kIframe = getIframeByWindow(e.source);
-    if (!c2kIframe) return;
-    var windowHeight = document.documentElement.clientHeight;
-    var iframeCoords = c2kIframe.elem.getBoundingClientRect();
-
-    var message = {type: 'c2k_iframe_position'};
-    message.scrollTop = iframeCoords.top < 0 ? -iframeCoords.top : 0;
-    message.height = Math.min(iframeCoords.bottom, windowHeight) - Math.max(iframeCoords.top, 0);
-    e.source.postMessage(JSON.stringify(message), '*');
-  };
-  var listener = function(e) {
-    if (e.data === 'c2k_embed_loaded') {
-      return loadIframe();
-    } else if (e.data === 'c2k_get_iframe_position') {
-      return iframePopupListener(e);
-    } else {
-      try {
-        var parsed = JSON.parse(e.data) || {};
-      } catch(err) {
-        return;
-      }
-      if (parsed.type === 'c2k_set_iframe_height') {
-        return iframeHeightListener(e, parsed.height);
-      }
-    }
-  };
-
-  if (window.addEventListener) {
-    window.addEventListener("message", listener);
-  } else {
-    window.attachEvent("onmessage", listener);
-  }
-  var URL = '192.168.100.3:8080/chiasmBin/embed.js', //without protocol
-    DOWNLOAD_LINK = '192.168.100.3:8080/',
-    frames = [];
-  window.__is_c2k_listener_set__ = true;
-  loadIframe();
-})();
+!function(){function e(e){this.elem=e,this.isHidden=!1,this.placeholder=null}if(window.__is_c2k_listener_set__)return void window.postMessage("c2k_embed_loaded","*")
+var t=function(e){return window.getComputedStyle?getComputedStyle(e,""):e.currentStyle}
+e.prototype.showIframePlaceholder=function(e){var i=this.placeholder
+i||(i=document.createElement("div"),i.style.border="3px dashed #adadad",i.style.boxSizing="border-box",i.style.backgroundColor="#f1f1f1",i.style.width=this.elem.style.width,this.placeholder=i),i.style.height=e,i.style.display="block"===t(this.elem).display?"block":"inline-block",this.elem.parentElement.insertBefore(i,this.elem)},e.prototype.hideIframePlaceholder=function(){this.placeholder&&this.placeholder.parentElement&&this.placeholder.parentElement.removeChild(this.placeholder)},e.prototype.hideIframe=function(){this.elem.style.height="0",this.isHidden=!0},e.prototype.showIframe=function(e){this.elem.style.height=e,this.isHidden=!1}
+var i=function(e){var t=document.createElement("iframe")
+return t.setAttribute("frameborder","0"),t.style.border="0",void 0!==e.width?t.style.width=e.width+("yes"===e.percent?"%":"px"):t.style.width="100%",t.src=e.protocol+"//"+m+e.id+".html?auto",t},r=function(e){e=decodeURIComponent(e)
+for(var t,i=/(\w+)=([^&]+)/g,r={};t=i.exec(e);)r[t[1]]=t[2]
+var o=e.match(/^https?:/)
+return r.protocol=o?o[0]:"",r},o=function(e,t){return void 0===t?e.c2k_is_loaded:void(e.c2k_is_loaded=t)},n=function(){for(var e=['script[src^="http://'+h+'"]','script[src^="https://'+h+'"]','script[src^="//'+h+'"]'],t=document.querySelectorAll(e.join(", ")),i=0;i<t.length;i++)if(!o(t[i]))return t[i]},s=function(){var t=n()
+if(t){o(t,!0)
+var s=r(t.src),l=new e(i(s))
+f.push(l),t.parentElement.insertBefore(l.elem,t)}},l=function(e){for(var t=0;t<f.length;t++)if(f[t].elem.contentWindow===e)return f[t]},d=function(e,t){var i=l(e.source)
+if(i)if("none"===t){var r=i.elem.style.height
+i.hideIframe(),i.showIframePlaceholder(r)}else i.isHidden?(i.hideIframePlaceholder(),i.showIframe(t)):i.elem.style.height=t},a=function(e){var t=l(e.source)
+if(t){var i=document.documentElement.clientHeight,r=t.elem.getBoundingClientRect(),o={type:"c2k_iframe_position"}
+o.scrollTop=r.top<0?-r.top:0,o.height=Math.min(r.bottom,i)-Math.max(r.top,0),e.source.postMessage(JSON.stringify(o),"*")}},c=function(e){if("c2k_embed_loaded"===e.data)return s()
+if("c2k_get_iframe_position"===e.data)return a(e)
+try{var t=JSON.parse(e.data)||{}}catch(i){return}return"c2k_set_iframe_height"===t.type?d(e,t.height):void 0}
+window.addEventListener?window.addEventListener("message",c):window.attachEvent("onmessage",c)
+var h="192.168.100.3:8080/chiasmBin/embed.js",m="192.168.100.3:8080/",f=[]
+window.__is_c2k_listener_set__=!0,s()}()
