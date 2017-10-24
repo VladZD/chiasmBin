a8662395aa7395df45fdb1920c262d86497b1e83
Vlad Drozd <vladbbird@gmail.com>
2017-10-24 08:40:44 +0300

    Add "scrolling: no" right to embed.js"

--- chiasmBin/embed.js
+++ chiasmBin/embed.js
@@ -3,7 +3,7 @@ var t=function(e){return window.getComputedStyle?getComputedStyle(e,""):e.curren
 e.prototype.showIframePlaceholder=function(e){var i=this.placeholder
 i||(i=document.createElement("div"),i.style.border="3px dashed #adadad",i.style.boxSizing="border-box",i.style.backgroundColor="#f1f1f1",i.style.width=this.elem.style.width,this.placeholder=i),i.style.height=e,i.style.display="block"===t(this.elem).display?"block":"inline-block",this.elem.parentElement.insertBefore(i,this.elem)},e.prototype.hideIframePlaceholder=function(){this.placeholder&&this.placeholder.parentElement&&this.placeholder.parentElement.removeChild(this.placeholder)},e.prototype.hideIframe=function(){this.elem.style.height="0",this.isHidden=!0},e.prototype.showIframe=function(e){this.elem.style.height=e,this.isHidden=!1}
 var i=function(e){var t=document.createElement("iframe")
-return t.setAttribute("frameborder","0"),t.style.border="0",void 0!==e.width?t.style.width=e.width+("yes"===e.percent?"%":"px"):t.style.width="100%",t.src=e.protocol+"//"+m+e.id+".html?auto",t},r=function(e){e=decodeURIComponent(e)
+return t.setAttribute("frameborder","0"),t.style.border="0",t.scrolling="no",void 0!==e.width?t.style.width=e.width+("yes"===e.percent?"%":"px"):t.style.width="100%",t.src=e.protocol+"//"+m+e.id+".html?auto",t},r=function(e){e=decodeURIComponent(e)
 for(var t,i=/(\w+)=([^&]+)/g,r={};t=i.exec(e);)r[t[1]]=t[2]
 var o=e.match(/^https?:/)
 return r.protocol=o?o[0]:"",r},o=function(e,t){return void 0===t?e.c2k_is_loaded:void(e.c2k_is_loaded=t)},n=function(){for(var e=['script[src^="http://'+h+'"]','script[src^="https://'+h+'"]','script[src^="//'+h+'"]'],t=document.querySelectorAll(e.join(", ")),i=0;i<t.length;i++)if(!o(t[i]))return t[i]},s=function(){var t=n()
